a452fd362abb1746fe553b8afe0d3ea783071c4c
Alansan <alaansannchezz@gmail.com>
2022-12-02 16:53:33 -0300

    Se configuro esLint

---
+++ src/router/Portfolio.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>portfolio</h1>
+</template>
+
+<script>
+export default {
+  name: 'Portfolio',
+  components: {
+    
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
