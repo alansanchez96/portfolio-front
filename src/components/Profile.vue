94060b5daef9152f52091aa2d5c0188c700235e4
Alan Sanchez <alaansannchezz@gmail.com>
2022-12-08 23:17:10 -0300

    Se añadio mi informacion de perfil

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -2,11 +2,11 @@
     <section class="page-section bg-dark text-white">
 
         <div class="container px-4 px-lg-5 text-center">
-            <img src="/assets/alansanchez.be1e2d89.jpeg" class="mx-auto rounded-circle"
-                style="width: 200px; height: 200px;" alt="">
+            <img src="https://i.ibb.co/drSQ8KZ/809ceffe-caa1-4bad-9f0c-c339cfa36f84.jpg" alt="Alan Sanchez"
+                class="mx-auto rounded-circle" style="width: 200px; height: 200px;">
             <br>
             <a class="btn btn-light btn-xl mt-5"
-                href="https://drive.google.com/file/d/1gZbS-Uq3EOjDfEY8HAJlEQmX6I9YeVPq/view" target="_blank">¡Observa
+                href="https://drive.google.com/file/d/1nXff77BiiKA7v-aFNT5nBvWMDLTrQWfJ/view" target="_blank">¡Observa
                 mi CV!</a>
 
         </div>
