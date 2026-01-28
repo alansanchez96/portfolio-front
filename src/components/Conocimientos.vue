3477522da699554b85adc3a20e259cd7fb026816
Alansan <alaansannchezz@gmail.com>
2026-01-28 15:13:11 -0300

    se eliminan los iconos por apirest y se añaden desde assets

--- src/components/Conocimientos.vue
+++ src/components/Conocimientos.vue
@@ -1,27 +1,347 @@
 <template>
     <section class="page-section" id="services">
         <div class="container px-4 px-lg-5">
-            <h2 class="text-center mt-0">Mis conocimientos tecnológicos en desarrollo web</h2>
+            <h2 class="text-center mt-0">Stack tecnológico</h2>
             <hr class="divider" />
             <div class="slider">
-                <p class="text-danger text-center fw-bold mx-auto" v-show="serverError">
+                <!-- <p class="text-danger text-center fw-bold mx-auto" v-show="serverError">
                     <span>Ocurrio un error con el servidor.</span><br>
                     <span class="text-primary">Ayudame a mantenerlo funcionando con una pequeña donacion a este Alias:</span><br><br>
                     <span class="text-success">OPENBANK.SNCHZ</span>
-                </p>
-                <div v-for="stack in stacks" :key="stack" v-show="showStacks">
+                </p> -->
+                <!-- <div v-for="stack in stacks" :key="stack" v-show="showStacks"> -->
                     <div class="card__conocimiento">
                         <div class="card__img">
-                            <img :src="(url + stack.attributes.image)" alt="">
+                            <img src="./../assets/img/conocimientos/php.png" alt="PHP">
                         </div>
-                        <div class="card__text" v-if="(stack.attributes.state == 0)">
-                            <p class="mt-3 text-secondary fw-bold">En proceso</p>
-                        </div>
-                        <div class="card__text" v-else-if="(stack.attributes.state == 1)">
-                            <p class="mt-3 text-success fw-bold">Dominado</p>
+                        <div class="card__text">
+                            <p class="mt-3 fw-bold">PHP</p>
                         </div>
                     </div>
-                </div>
+                    <div class="card__conocimiento">
+                        <div class="card__img">
+                            <img src="./../assets/img/conocimientos/laravel.png" alt="Laravel">
+                        </div>
+                        <div class="card__text">
+                            <p class="mt-3 fw-bold">Laravel</p>
+                        </div>
+                    </div>
+                    <div class="card__conocimiento">
+                        <div class="card__img">
+                            <img src="./../assets/img/conocimientos/mysql.png" alt="mysql">
+                        </div>
+                        <div class="card__text">
+                            <p class="mt-3 fw-bold">MySQL</p>
+                        </div>
+                    </div>
+                    <div class="card__conocimiento">
+                        <div class="card__img">
+                            <img src="./../assets/img/conocimientos/mongodb.png" alt="mongodb">
+                        </div>
+                        <div class="card__text">
+                            <p class="mt-3 fw-bold">MongoDB</p>
+                        </div>
+                    </div>
+                    <div class="card__conocimiento">
+                        <div class="card__img">
+                            <img src="./../assets/img/conocimientos/docker.png" alt="DOCKER">
+                        </div>
+                        <div class="card__text">
+                            <p class="mt-3 fw-bold">Docker</p>
+                        </div>
+                    </div>
+                    <div class="card__conocimiento">
+                        <div class="card__img">
+                            <img src="./../assets/img/conocimientos/vuejs.png" alt="vuejs">
+                        </div>
+                        <div class="card__text">
+                            <p class="mt-3 fw-bold">VueJS</p>
+                        </div>
+                    </div>
+                    <div class="card__conocimiento">
+                        <div class="card__img">
+                            <img src="./../assets/img/conocimientos/nginx.png" alt="nginx">
+                        </div>
+                        <div class="card__text">
+                            <p class="mt-3 fw-bold">Nginx</p>
+                        </div>
+                    </div>
+                    <div class="card__conocimiento">
+                        <div class="card__img">
+                            <img src="./../assets/img/conocimientos/git.webp" alt="git">
+                        </div>
+                        <div class="card__text">
+                            <p class="mt-3 fw-bold">GIT</p>
+                        </div>
+                    </div>
+                    <div class="card__conocimiento">
+                        <div class="card__img">
+                            <img src="./../assets/img/conocimientos/html.png" alt="html">
+                        </div>
+                        <div class="card__text">
+                            <p class="mt-3 fw-bold">HTML</p>
+                        </div>
+                    </div>
+                    <div class="card__conocimiento">
+                        <div class="card__img">
+                            <img src="./../assets/img/conocimientos/javascript.png" alt="javascript">
+                        </div>
+                        <div class="card__text">
+                            <p class="mt-3 fw-bold">Javascript</p>
+                        </div>
+                    </div>
+                    <div class="card__conocimiento">
+                        <div class="card__img">
+                            <img src="./../assets/img/conocimientos/nodejs.png" alt="NodeJS Node js node.js">
+                        </div>
+                        <div class="card__text">
+                            <p class="mt-3 fw-bold">NodeJS</p>
+                        </div>
+                    </div>
+                    <div class="card__conocimiento">
+                        <div class="card__img">
+                            <img src="./../assets/img/conocimientos/express.png" alt="express expressjs">
+                        </div>
+                        <div class="card__text">
+                            <p class="mt-3 fw-bold">ExpressJS</p>
+                        </div>
+                    </div>
+                    <div class="card__conocimiento">
+                        <div class="card__img">
+                            <img src="./../assets/img/conocimientos/css.png" alt="css">
+                        </div>
+                        <div class="card__text">
+                            <p class="mt-3 fw-bold">CSS</p>
+                        </div>
+                    </div>
+                    <div class="card__conocimiento">
+                        <div class="card__img">
+                            <img src="./../assets/img/conocimientos/bootstrap.png" alt="bootstrap">
+                        </div>
+                        <div class="card__text">
+                            <p class="mt-3 fw-bold">Bootstrap</p>
+                        </div>
+                    </div>
+                    <div class="card__conocimiento">
+                        <div class="card__img">
+                            <img src="./../assets/img/conocimientos/tailwind.png" alt="tailwind">
+                        </div>
+                        <div class="card__text">
+                            <p class="mt-3 fw-bold">TailwindCSS</p>
+                        </div>
+                    </div>
+                    <div class="card__conocimiento">
+                        <div class="card__img">
+                            <img src="./../assets/img/conocimientos/sass.webp" alt="sass scss">
+                        </div>
+                        <div class="card__text">
+                            <p class="mt-3 fw-bold">SASS</p>
+                        </div>
+                    </div>
+                    <div class="card__conocimiento">
+                        <div class="card__img">
+                            <img src="./../assets/img/conocimientos/vite.png" alt="vite">
+                        </div>
+                        <div class="card__text">
+                            <p class="mt-3 fw-bold">Vite</p>
+                        </div>
+                    </div>
+                    <div class="card__conocimiento">
+                        <div class="card__img">
+                            <img src="./../assets/img/conocimientos/symfony.png" alt="symfony">
+                        </div>
+                        <div class="card__text">
+                            <p class="mt-3 fw-bold">Symfony</p>
+                        </div>
+                    </div>
+                    <div class="card__conocimiento">
+                        <div class="card__img">
+                            <img src="./../assets/img/conocimientos/jquery.png" alt="jquery">
+                        </div>
+                        <div class="card__text">
+                            <p class="mt-3 fw-bold">jQuery</p>
+                        </div>
+                    </div>
+                    <div class="card__conocimiento">
+                        <div class="card__img">
+                            <img src="./../assets/img/conocimientos/redis.png" alt="redis">
+                        </div>
+                        <div class="card__text">
+                            <p class="mt-3 fw-bold">REDIS</p>
+                        </div>
+                    </div>
+                    <div class="card__conocimiento">
+                        <div class="card__img">
+                            <img src="./../assets/img/conocimientos/phpunit.png" alt="phpunit">
+                        </div>
+                        <div class="card__text">
+                            <p class="mt-3 fw-bold">PHPUnit</p>
+                        </div>
+                    </div>
+                    <div class="card__conocimiento">
+                        <div class="card__img">
+                            <img src="./../assets/img/conocimientos/linux.png" alt="linux">
+                        </div>
+                        <div class="card__text">
+                            <p class="mt-3 fw-bold">LinuxOS</p>
+                        </div>
+                    </div>
+                    <div class="card__conocimiento">
+                        <div class="card__img">
+                            <img src="./../assets/img/conocimientos/ssh.png" alt="ssh">
+                        </div>
+                        <div class="card__text">
+                            <p class="mt-3 fw-bold">SSH</p>
+                        </div>
+                    </div>
+                    <div class="card__conocimiento">
+                        <div class="card__img">
+                            <img src="./../assets/img/conocimientos/bash.png" alt="bash">
+                        </div>
+                        <div class="card__text">
+                            <p class="mt-3 fw-bold">BASH</p>
+                        </div>
+                    </div>
+                    <div class="card__conocimiento">
+                        <div class="card__img">
+                            <img src="./../assets/img/conocimientos/postman.png" alt="postman">
+                        </div>
+                        <div class="card__text">
+                            <p class="mt-3 fw-bold">Postman</p>
+                        </div>
+                    </div>
+                    <div class="card__conocimiento">
+                        <div class="card__img">
+                            <img src="./../assets/img/conocimientos/insomnia.png" alt="insomnia">
+                        </div>
+                        <div class="card__text">
+                            <p class="mt-3 fw-bold">Insomnia</p>
+                        </div>
+                    </div>
+                    <div class="card__conocimiento">
+                        <div class="card__img">
+                            <img src="./../assets/img/conocimientos/figma.png" alt="figma">
+                        </div>
+                        <div class="card__text">
+                            <p class="mt-3 fw-bold">Figma</p>
+                        </div>
+                    </div>
+                    <div class="card__conocimiento">
+                        <div class="card__img">
+                            <img src="./../assets/img/conocimientos/gcp.png" alt="gcp">
+                        </div>
+                        <div class="card__text">
+                            <p class="mt-3 fw-bold">GoogleCloud</p>
+                        </div>
+                    </div>
+                    <div class="card__conocimiento">
+                        <div class="card__img">
+                            <img src="./../assets/img/conocimientos/digitalocean.png" alt="digitalocean digital ocean">
+                        </div>
+                        <div class="card__text">
+                            <p class="mt-3 fw-bold">DigitalOcean</p>
+                        </div>
+                    </div>
+                    <div class="card__conocimiento">
+                        <div class="card__img">
+                            <img src="./../assets/img/conocimientos/heroku.png" alt="heroku">
+                        </div>
+                        <div class="card__text">
+                            <p class="mt-3 fw-bold">Heroku</p>
+                        </div>
+                    </div>
+                    <div class="card__conocimiento">
+                        <div class="card__img">
+                            <img src="./../assets/img/conocimientos/aws.png" alt="aws">
+                        </div>
+                        <div class="card__text">
+                            <p class="mt-3 fw-bold">AWS</p>
+                        </div>
+                    </div>
+                    <div class="card__conocimiento">
+                        <div class="card__img">
+                            <img src="./../assets/img/conocimientos/openai.png" alt="openai">
+                        </div>
+                        <div class="card__text">
+                            <p class="mt-3 fw-bold">OpenAI</p>
+                        </div>
+                    </div>
+                    <div class="card__conocimiento">
+                        <div class="card__img">
+                            <img src="./../assets/img/conocimientos/meta.png" alt="meta">
+                        </div>
+                        <div class="card__text">
+                            <p class="mt-3 fw-bold">META.Developers</p>
+                        </div>
+                    </div>
+                    <div class="card__conocimiento">
+                        <div class="card__img">
+                            <img src="./../assets/img/conocimientos/sentry.png" alt="sentry">
+                        </div>
+                        <div class="card__text">
+                            <p class="mt-3 fw-bold">Sentry</p>
+                        </div>
+                    </div>
+                    <div class="card__conocimiento">
+                        <div class="card__img">
+                            <img src="./../assets/img/conocimientos/pusher.png" alt="pusher">
+                        </div>
+                        <div class="card__text">
+                            <p class="mt-3 fw-bold">Pusher</p>
+                        </div>
+                    </div>
+                    <div class="card__conocimiento">
+                        <div class="card__img">
+                            <img src="./../assets/img/conocimientos/stripe.png" alt="stripe">
+                        </div>
+                        <div class="card__text">
+                            <p class="mt-3 fw-bold">Stripe</p>
+                        </div>
+                    </div>
+                    <div class="card__conocimiento">
+                        <div class="card__img">
+                            <img src="./../assets/img/conocimientos/mp.png" alt="mp">
+                        </div>
+                        <div class="card__text">
+                            <p class="mt-3 fw-bold">MercadoPago.Developers</p>
+                        </div>
+                    </div>
+                    <div class="card__conocimiento">
+                        <div class="card__img">
+                            <img src="./../assets/img/conocimientos/paypal.png" alt="paypal">
+                        </div>
+                        <div class="card__text">
+                            <p class="mt-3 fw-bold">Paypal</p>
+                        </div>
+                    </div>
+                    <div class="card__conocimiento">
+                        <div class="card__img">
+                            <img src="./../assets/img/conocimientos/jira.png" alt="jira">
+                        </div>
+                        <div class="card__text">
+                            <p class="mt-3 fw-bold">JIRA</p>
+                        </div>
+                    </div>
+                    <div class="card__conocimiento">
+                        <div class="card__img">
+                            <img src="./../assets/img/conocimientos/ssh.png" alt="ssh">
+                        </div>
+                        <div class="card__text">
+                            <p class="mt-3 fw-bold">SSH</p>
+                        </div>
+                    </div>
+                    <div class="card__conocimiento">
+                        <div class="card__img">
+                            <img src="./../assets/img/conocimientos/cypress.png" alt="cypress">
+                        </div>
+                        <div class="card__text">
+                            <p class="mt-3 fw-bold">Cypress</p>
+                        </div>
+                    </div>
+
+                    
+                    
+                <!-- </div> -->
             </div>
         </div>
     </section>
