31af0b42dbdafccfceb270a0db013b672a8f4714
Alansan <alaansannchezz@gmail.com>
2026-02-03 20:01:41 -0300

    renovacion de data para Profile

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -2,7 +2,7 @@
     <section class="page-section bg-dark text-white">
 
         <div class="container px-4 px-lg-5 text-center">
-            <img src="https://i.ibb.co/b1g7fvy/Picsart-23-10-30-15-19-14-576.jpg" alt="Alan Sanchez"
+            <img src="https://i.ibb.co/mFCzyDRY/Whats-App-Image-2025-12-21-at-9-42-02-PM.jpg" alt="Alan Sanchez"
                 class="mx-auto rounded-circle" style="width: 200px; height: 200px;" border="0">
             <br>
             <a class="btn btn-light btn-xl mt-5" :href="url_pdf" target="_blank">
@@ -17,7 +17,7 @@
 export default {
     data() {
         return {
-            'url_pdf': '',
+            'url_pdf': 'https://drive.google.com/file/d/1QRjhqs4O07rjaLMG3opj_CGTsHBnrJzk/view?usp=sharing',
         }
     }
 }
