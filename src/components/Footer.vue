6ab4e3e4f81a79d1903f4c2abd6bd0ba0c0a209e
Alansan <alaansannchezz@gmail.com>
2026-02-05 18:02:43 -0300

    actualizando footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,9 +1,9 @@
 <template>
     <footer class="py-5">
         <div class="container px-4 px-lg-5">
-            <div class="small text-center text-primary">Copyright &copy; 2022 - <a
+            <div class="small text-center text-muted">SALMOS 23:1 «Jehová es mi pastor; nada me faltará.»</div>
+            <div class="small text-center text-primary">Copyright &copy; 2026 — <a
                     href="https://linkedin.com/in/alansanchez96" class="">Alan Sanchez</a></div>
-            <div class="small text-center text-muted">Portfolio @ API Restful</div>
         </div>
     </footer>
 </template>
